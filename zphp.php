4e76826875e6b1d96fbfef902846e18de82709fd
ducdevweb <duclmps34770@fpt.edu.vn>
2025-04-06 19:50:22 +0700

    Delete zphp.php

--- zphp.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$i = [2, 2, 5, 4, 85, 4];
-sort($i);
-echo "Sắp xếp tăng dần: " . implode(", ", $i) . "\n";
-$unique = array_unique($i);
-echo "Mảng không trùng lặp: " . implode(", ", $unique) . "\n";
-?>
